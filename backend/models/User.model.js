1b1383b8492420985baa82f3805488a0bd7c6c89
blaze <hardikkamaliya0000@gmail.com>
2025-12-06 17:02:58 +0530

    creating user model

---
+++ backend/models/User.model.js
@@ -0,0 +1,23 @@
+const userSchma = new mongoose.Schema({
+    fullname: { 
+        type: String, 
+        required: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+    },
+    mobile: { 
+        type: String, 
+        required: true
+    }, 
+    role:{
+        type: String,
+        enum: ["user", "owner", "deliveryboy"],
+        required: true,
+    }
+}, { timestamps: true });
